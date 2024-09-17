a4a37fc1d8cef57523645a55a125c4ace9b1029e
Barry Irvine <66362853+barry-irvine@users.noreply.github.com>
2024-09-17 16:05:59 +0100

    Release 1.3.0 (#2)
    Upgrade Compose to 1.7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project
 adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.3.0] - 2024-09-17
+
+### Changed
+
+Upgraded to the latest version of Compose, Kotlin etc.
+
 ## [1.2.0] - 2024-08-15
 
 ### Added
--- countrypicker/src/main/kotlin/com/gocity/countrypicker/ui/LargeBottomMenu.kt
+++ countrypicker/src/main/kotlin/com/gocity/countrypicker/ui/LargeBottomMenu.kt
@@ -1,7 +1,6 @@
 package com.gocity.countrypicker.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -106,7 +104,7 @@ internal fun <T> LargeBottomMenu(
             onDismissRequest = { expanded = false },
             sheetState = bottomSheetState,
             dragHandle = null,
-            windowInsets = WindowInsets.ime
+            contentWindowInsets = { WindowInsets.ime }
         ) {
             val listState = rememberLazyListState()
             if (currentItem != null) {
